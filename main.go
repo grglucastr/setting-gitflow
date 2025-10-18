c9aea296eb3790a200f1259ad618d83e9d692f3f
grglucastr <georgebnunes@gmail.com>
2025-10-18 12:21:11 -0300

    feat: add simple server container

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
+
+	server := http.NewServeMux()
+
 	fmt.Println("Hello World!!!")
+
+	server.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+
+		body := map[string]string{
+			"status": "UP",
+		}
+
+		resp, err := json.Marshal(body)
+
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write(resp)
+
+	})
+
+	err := http.ListenAndServe(":4000", server)
+
+	if err != nil {
+		os.Exit(1)
+	}
 }
